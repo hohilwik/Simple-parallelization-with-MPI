0fd33892ccd375b0143f67f66b43323db553cdb6
Shirley <80057848+hohilwik@users.noreply.github.com>
2022-08-30 11:15:52 +0530

    pi done

--- calc-pi/parallel_pi.c
+++ calc-pi/parallel_pi.c
@@ -3,34 +3,46 @@
 
 #include <stdio.h>
 #include <mpi.h>
+#include <math.h>
+//#include <limits.h>
 
-#define N 1E7
 // take a small function to calculate N and stuff first, based on accuracy required
-// let's say accuracy is 1E-14, find N such that 1/N < 1E-14 
+// let's say accuracy is 1E-14, find N such that 1/N^2 < 1E-14 
 
 int main (int argc, char* argv[])
 {
-    int rank, size, error, i;
-    double pi=0.0, result=0.0, sum=0.0, begin=0.0, end=0.0;
+	double N=1E4;
+	double accuracy;
+	
+    int rank, size, error;
+	long long int i;
+    double pi=0.0, result=0.0, sum=0.0, begin=0.0, end=0.0, delta=1E-14;
     
     error=MPI_Init (&argc, &argv);
     
     //Get process info
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
+	
+	if(rank==0)
+	{
+		scanf("%lf", &accuracy);
+		N = 1.0/accuracy;
+		if( N< 1E4 )
+		{
+			N=1E4;
+		}
+	}
+    
     
     //Synchronize all processes and get the begin time
     MPI_Barrier(MPI_COMM_WORLD);
     begin = MPI_Wtime();
-    
+	
     //Each process will caculate a part of the sum
     for (i=rank; i<N; i+=size)
     {
-        if(rank==0)
-		{
-			break;
-		}
-		result+=1.0/(i*i);
+		result+=1.0/( (i+1)*(i+1) );
     }
     
     //Sum up all results
@@ -44,7 +56,7 @@ int main (int argc, char* argv[])
     if (rank==0)
     {
         pi=sqrt(sum*6);
-        printf("np=%2d;    Time=%fs;    PI=%lf\n", size, end-begin, pi);
+        printf("np=%2d;    Time=%fs;    PI=%lf, N=%lf\n", size, end-begin, pi, N);
     }
     
     error=MPI_Finalize();
